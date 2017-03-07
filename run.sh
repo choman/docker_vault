0a748969940cb02f125edc4f8d801bc0f5b4ec91
choman <choman@localhost.localdomain>
2017-03-07 14:50:56 -0600

    initial commit

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker run --rm --cap-add=IPC_LOCK             \
+           -e VAULT_ADDR=http://127.0.0.1:8200 \
+           -v $(pwd)/config:/vault/config      \
+           -v $(pwd)/data:/vault/file          \
+           -p 8200:8200 --name vault vault server
